2fe3de501949ad57d07e67f00dee93bbc611744a
Timothy Ng <timnjj@gmail.com>
2016-04-29 09:07:39 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+  $(function(){
+      $(".cycle").typed({
+        strings: ["learner.", "video game fan.", "car enthusiast.", "fitness lover.", "programmer."],
+        typeSpeed: 50,
+        backDelay: 1000,
+        loop: true,
+        startDelay: 500
+      });
+  });
